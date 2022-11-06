6bd4246a8bf1d53e9d4672d16388b6aca9cfc8af
Martin Hoppe <martin@2x2hoppe.de>
2022-11-06 22:03:54 +0100

    method added

--- src/ErrorHandling/index.ts
+++ src/ErrorHandling/index.ts
@@ -5,5 +5,11 @@ export default {
     }
     return error.response.status === 409 &&
     error.response.data.code === 'DesignDataVersionConflict'
+  },
+  notModified: (error): boolean => {
+    if (!error || !error.response){
+      return false
+    }
+    return error.response.status === 304
   }
 }

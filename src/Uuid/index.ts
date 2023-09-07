dcfb0a6e9995e8c786f705685106f222c4f48f83
Martin Hoppe <48030323+ma67ho@users.noreply.github.com>
2023-09-07 17:26:55 +0200

    method temporary() added

--- src/Uuid/index.ts
+++ src/Uuid/index.ts
@@ -2,26 +2,46 @@
  * @typedef {string} uuid A string containing an UUID encapsulated by {}
  */
 
- import { v4 as uuidv4 } from 'uuid'
+import { v4 as uuidv4 } from 'uuid'
 
- /**
-  * Generates an UUID
-  * @function generate
-  * @returns {uuid} An UUID
-  * @since 0.1.0
-  * @memberof module:Utils/Uuid
-  */
-  function generate (): string {
-   return '{' + uuidv4() + '}'
- }
- 
- /**
- * @param  {uuid} uuid A string containing an UUID
- * @returns {boolean} Returns true if the string object contains a valid UUID; otherwise false.
+/**
+ * Generates an UUID
+ * @function generate
+ * @returns {uuid} An UUID
  * @since 0.1.0
  * @memberof module:Utils/Uuid
  */
- function isUuid(uuid: string): boolean {
+function generate(): string {
+  return '{' + uuidv4() + '}'
+}
+
+function temporary(): string {
+  return '{T-' + uuidv4() + '}'
+}
+
+function isTemporary(uuid: string): boolean {
+  if (typeof uuid !== 'string') {
+    return false
+  }
+  if (uuid.length !== 40) {
+    return false
+  }
+  if (!uuid.startsWith('{T-')) {
+    return false
+  }
+  if (!uuid.endsWith('}')) {
+    return false
+  }
+  return true
+
+}
+/**
+* @param  {uuid} uuid A string containing an UUID
+* @returns {boolean} Returns true if the string object contains a valid UUID; otherwise false.
+* @since 0.1.0
+* @memberof module:Utils/Uuid
+*/
+function isUuid(uuid: string): boolean {
   if (typeof uuid !== 'string') {
     return false
   }
@@ -42,5 +62,7 @@
  */
 export default {
   generate,
-  isUuid
+  isTemporary,
+  isUuid,
+  temporary
 }

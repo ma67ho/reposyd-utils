f66d3ea779c5d1eede20061988fe7670d0aafea4
Martin Hoppe <48030323+ma67ho@users.noreply.github.com>
2024-01-24 14:08:33 +0100

    result undefined throw exception

--- src/DataAnalysis/filterResults.ts
+++ src/DataAnalysis/filterResults.ts
@@ -5,29 +5,10 @@ import ConditionParser from "./ConditionParser"
 
 export default function (key, group, results) {
   if (group.condition === undefined) {
-    return results.filter(result => result !== null && result.props[key] === group.key)
+    return results.filter(result => result !== null && result.props !== undefined && result.props[key] === group.key)
   }
-  // const conditions = build(ConditionParser.parse(group.condition))
   const condition = build(ConditionParser.parse(group.condition))
   return results.filter(result => {
-    // eslint-disable-next-line no-debugger
-    // debugger;
     return condition(key, group, result)
   })
-
-  // if (Array.isArray(f.subject)) {
-  //   if (f.subject[0] === '$property') {
-  //     if (f.operator === 'eq') {
-  //       conditions.push((key, group, result) => result.result !== null && result.props[f.subject[1]] === f.value)
-  //     }
-  //   }
-  // } else if (f.subject === '$result') {
-  //   if (f.operator === 'eq') {
-  //     conditions.push((key, group, result) => result.result === f.value)
-  //   } else if (f.operator === 'ne') {
-  //     conditions.push((key, group, result) => result.result !== f.value)
-  //   }
-  // }
-
-  // return results.filter(result => match(key, group, conditions, result))
 }
